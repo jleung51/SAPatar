d8ee0cbae87d4f47ed0c7091f062b93dbcc032a1
Zhi Guo <zhiguo@Zhis-MacBook-Pro.local>
2020-07-17 11:51:59 -0700

    Finished setting up dev env

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin(
+            {
+                template: './src/index.html',
+                filename: 'index.html'
+            }
+        )
+    ],
+}
