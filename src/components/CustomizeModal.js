c9c8c081debd486b71f3d6202933e97bbd6f45f1
Zhi Guo <zhiguo@Zhis-MacBook-Pro.local>
2020-07-24 22:41:38 -0700

    made clickable assets

--- src/components/CustomizeModal.js
+++ src/components/CustomizeModal.js
@@ -70,14 +70,34 @@ function CustomizeModal(){
                 <Button variant="outline-light">
                 <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
                 </Button>
-                <Piece pieceType="eyes" pieceSize="100" eyeType="Dizzy"/>
-                <Piece pieceType="eyebrows" pieceSize="100" eyebrowType="RaisedExcited"/>
-                <Piece pieceType="accessories" pieceSize="100" accessoriesType="Round"/>
-                <Piece pieceType="top" pieceSize="100" topType="LongHairFro" hairColor="Red"/>
-                <Piece pieceType="facialHair" pieceSize="100" facialHairType="BeardMajestic"/>
-                <Piece pieceType="clothe" pieceSize="100" clotheType="Hoodie" clotheColor="Red"/>
-                <Piece pieceType="graphics" pieceSize="100" graphicType="Skull" />
-                <Piece pieceType="skin" pieceSize="100" skinColor="Brown" />
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
+
+                <Button variant="outline-light">
+                <Piece pieceType="mouth" pieceSize="100" mouthType="Eating"/>
+                </Button>
                 </Container>
               </Col>
             </Row>
