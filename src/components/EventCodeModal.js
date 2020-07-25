81df03a1b1fad3c78f268c5a47e4a6347c9878ed
Jeffrey Leung <jleung513@gmail.com>
2020-07-25 11:22:27 -0700

    Fix indentation for return

--- src/components/EventCodeModal.js
+++ src/components/EventCodeModal.js
@@ -54,48 +54,48 @@ function EventCodeModal(){
   Object.assign(greyButtonSmall, styles.orangeButtonSmall);
   greyButtonSmall.background = 'linear-gradient(3.76deg, #727C7B 5.48%, #AFB8B7 67.63%)';
 
-    return (
-      <div>
-        <Button onClick={openModal}
-          style={styles.orangeButtonLarge} >
-            I attended<br/>an Event
-        </Button>
+  return (
+    <div>
+      <Button onClick={openModal}
+        style={styles.orangeButtonLarge} >
+          I attended<br/>an Event
+      </Button>
 
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Event Code Modal"
-        >
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Event Code Modal"
+      >
 
-          <h2 ref={_subtitle => (subtitle = _subtitle)}></h2>
-          <div style={styles.eventModal.container}>
-            <div style={styles.eventModal.title}>
-              Code Entry
-            </div>
-
-            <form>
-              <label style={styles.eventModal.prompt}>
-              Enter your event code:
-                <input type="text" name="eventCode" style={styles.eventModal.input} />
-              </label>
-            </form>
-
-            <Button onClick={closeModal}
-              style={greyButtonSmall} >
-              Cancel
-            </Button>
-
-            <Button onClick={submitCode}
-              style={styles.orangeButtonSmall} >
-              Submit Code
-            </Button>
-
+        <h2 ref={_subtitle => (subtitle = _subtitle)}></h2>
+        <div style={styles.eventModal.container}>
+          <div style={styles.eventModal.title}>
+            Code Entry
           </div>
-        </Modal>
-      </div>
-    );
+
+          <form>
+            <label style={styles.eventModal.prompt}>
+            Enter your event code:
+              <input type="text" name="eventCode" style={styles.eventModal.input} />
+            </label>
+          </form>
+
+          <Button onClick={closeModal}
+            style={greyButtonSmall} >
+            Cancel
+          </Button>
+
+          <Button onClick={submitCode}
+            style={styles.orangeButtonSmall} >
+            Submit Code
+          </Button>
+
+        </div>
+      </Modal>
+    </div>
+  );
 }
 
 export default EventCodeModal
